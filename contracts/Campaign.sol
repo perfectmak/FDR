e3c035a20b72809ec1af19506293ff981812420a
Oyewale Ademola <saopayne@gmail.com>
2017-07-24 08:22:03 +0100

    - Update method names - Add refund method for an an address - Add structure to hold the contributors in memory

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,113 +1,198 @@
 pragma solidity ^0.4.0;
 
-
 contract Campaign {
 
-    // the individual raising funds
-    address public raiser;
-    // the array of individuals who have donated to this fundraising
-    mapping (address => uint) public donorsPaid;
-    // total number of Donors so far
-    uint public numDonors;
-    // the deadline
-    uint public fundDeadline;
-    // the target to be met with this fundraising
-    uint public fundTarget;
-    // Set to true at the end, disallows any change
-    bool ended;
+    address public beneficiary;
 
+    uint public fundingGoal;
+    uint public amountRaised;
+    uint public deadline;
+    uint public price;
+
+    mapping(address => uint256) public balanceOf;
+
+    bool fundingGoalReached = false;
     // Events that will be fired on changes.
-    event FundRaiseEnded(address raiser, uint _amount);
-    event FundRaiseTargetMet(address raiser, uint _amount);
-    event Deposit(address _from, uint _amount); // log the deposit
+    event GoalReached(address beneficiary, uint amountRaised);
+    event FundTransfer(address backer, uint amount, bool isContribution); // log the fund transfer
     event Refund(address _to, uint _amount); // possible refund if the target is not met
 
-    function Campaign() {
-        raiser = msg.sender;
-        fundTarget = 100;
-        numDonors = 0;
+    bool campaignClosed = false;
+
+    /* data structure to hold information about campaign contributors */
+    struct Contributor {
+        uint received;
+        uint returned;
+        uint contributorListPointer;
     }
 
-    // contribute to ongoing fund raise
-    function contributeToFundRaise() public {
-        // check if deadline hasn't been exceeded
-        if (now > fundDeadline ) {
-            throw; // throw ensures funds will be returned
+    // the array of individuals who have donated to this fundraising
+    mapping(address => Contributor) public contributorStructs;
+
+    address[] public contributorList;
+
+    /*  at initialization, setup the owner */
+    function Campaign(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint durationInMinutes,
+        uint etherCostOfEachToken,
+        token addressOfTokenUsedAsReward
+    ) {
+        beneficiary = ifSuccessfulSendTo;
+        fundingGoal = fundingGoalInEthers * 1 ether;
+        deadline = now + durationInMinutes * 1 minutes;
+        // we will modify this along the line
+        price = etherCostOfEachToken * 1 ether;
+    }
+
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    modifier afterDeadline() {
+        if (now >= deadline)
+        _;
+    }
+
+    function getContributorCount()
+        public
+        constant
+        returns(uint contributorCount)
+    {
+        return contributorList.length;
+    }
+
+    function isContributor(address contributor)
+        public
+        constant
+        returns(bool isIndeed)
+    {
+        if(contributorList.length==0) return false;
+        return contributorList[contributorStructs[contributor].contributorListPointer]==contributor;
+    }
+
+
+    /* The function that is called whenever anyone sends funds to a contract */
+    function contribute() payable
+        condition(msg.value > 0.00000000)
+        returns(bool success)
+    {
+        // don't accept transaction if the campaign has closed
+        if (campaignClosed){
+            throw;
         }
+        // Revert the call to contribute if the fund raising
+        // period is over
+        require(now <= deadline);
+
+        // push new contributor, update existing
+        if(!isContributor(msg.sender)) {
+            contributorStructs[msg.sender].contributorListPointer = contributorList.push(msg.sender)-1;
+        }
+
+        uint amount = msg.value;
+        balanceOf[msg.sender] = amount;
+        amountRaised += amount;
+
         donorsPaid[msg.sender] = msg.value;
-        numDonors++;
-        Deposit(msg.sender, msg.value);
-    }
 
-    // get back donation that was made to the campaign
-    function withdrawDonation() returns (bool) {
-        var amount = pendingReturns[msg.sender];
-        if (amount > 0) {
-            /**
-              * It is important to set this to zero because the recipient
-              * can call this function again as part of the receiving call
-              * before `send` returns.
-              */
-            pendingReturns[msg.sender] = 0;
-            if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
-        }
+        // keep track of receipt for contributors
+        contributorStructs[msg.sender].received += msg.value;
+        FundTransfer(msg.sender, amount, true);
         return true;
     }
 
-    // either reduce or increase the set deadline for the fund raise
-    function changeDeadline() private {
-
-    }
-
-    /** @dev change the target amount to meet for the fund raise
-     * @param amount the new amount to be set as target
-     */
-    function changeFundTarget(uint amount) private {
-        if (msg.sender != raiser) {
-            return;
-        }
-        // set the new target to be the new amount
-        fundTarget = amount;
-    }
 
     /*
      refund the funds to the donors if the target is not met or the fund raise is cancelled
      or flagged not to be genuine
     */
-    function refundDonation(address recipient, uint amount) public {
-        if (msg.sender != raiser) {
+    function refund(uint amount) public {
+        if (msg.sender != beneficiary) {
             return;
         }
-        if (donorsPaid[recipient] == amount) {
-            address myAddress = this;
-            if (myAddress.balance >= amount) {
-                recipient.send(amount);
-                Refund(recipient, amount);
-                donorsPaid[recipient] = 0;
-                numDonors--;
+        uint currentBalance = balanceOf[msg.sender];
+        balanceOf[msg.sender] = 0;
+        if (currentBalance > 0 && currentBalance >= amount) {
+            if (msg.sender.send(amount)) {
+                FundTransfer(msg.sender, amount, false);
+            } else {
+                // update the balance for the contributor
+                balanceOf[msg.sender] = currentBalance - amount;
             }
         }
         return;
     }
 
-    // retrieve the total amount donated thus far
-    function getTotalDonation() public {
+    /* checks if the goal or time limit has been reached and ends the campaign */
+    function checkGoalReached() afterDeadline {
+        if (amountRaised >= fundingGoal){
+            fundingGoalReached = true;
+            GoalReached(beneficiary, amountRaised);
+        }
+        campaignClosed = true;
+    }
 
+    // either reduce or increase the set deadline for the fund raise
+    function changeDeadline(uint newDeadline) private
+        returns(bool success)
+    {
+        if(now >= newDeadline || deadline >= newDeadline) {
+            return false;
+        }
+        // update the existing deadline
+        deadline = newDeadline;
+        return true;
+    }
+
+
+    function withdraw() afterDeadline {
+        if (!fundingGoalReached) {
+            uint amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                if (msg.sender.send(amount)) {
+                    FundTransfer(msg.sender, amount, false);
+                } else {
+                    balanceOf[msg.sender] = amount;
+                }
+            }
+        }
+
+        if (fundingGoalReached && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised)) {
+                FundTransfer(beneficiary, amountRaised, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock contributors balance
+                fundingGoalReached = false;
+            }
+        }
+    }
+
+    // retrieve the total amount donated thus far
+    function getTotalDonation()
+        public
+        constant
+        returns(uint)
+    {
+        return amountRaised;
     }
 
     // retrieve the addresses of donors on this fund raise
-    function getDonors() public {
-
+    function getDonors()
+        public
+        constant
+        returns(address[])
+    {
+        return contributorList;
     }
 
     // release funds in the contract to avoid permanent lockdown
     function destroy() {
-        if(msg.sender == raiser) {
-            suicide(raiser);
+        if(msg.sender == beneficiary) {
+            selfdestruct(beneficiary);
         }
     }
 
